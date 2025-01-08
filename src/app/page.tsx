ce81c13a3d5b7f9be759996e2908f33397f7d91c
frn-af <91farhanalfathra@gmail.com>
2025-01-08 14:48:06 +0700

    chore: refactoring main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,34 @@
-"use client"
+"use client";
 import { Crud } from "@/components/crud";
 import { Result } from "@/components/result-dialog";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Data } from "@/lib/db/schema";
-import { Linefont } from "next/font/google";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false)
-  const [formData, setFormData] = useState<Data>()
+  const [isOpen, setIsOpen] = useState(false);
+  const [formData, setFormData] = useState<Data | undefined>();
 
   const handleOpen = (data: Data) => {
-    setFormData(data)
-    setIsOpen(true)
-  }
+    setFormData(data);
+    setIsOpen(true);
+  };
+
   const handleClose = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
+
   return (
     <main className="h-full max-w-screen-md mx-auto p-10">
       <div className="w-full flex justify-center items-center h-full p-4">
         <Card className="w-2/3">
           <CardHeader className="flex justify-between w-full">
-            <div className="flex justify-between">
-              <h1>
-                Cek IMT Anda
-              </h1>
-              <Link href={"/imt"}>
-                <Button className="capitalize">
-                  lihat data lain
-                </Button>
+            <div className="flex justify-between w-full">
+              <h1>Cek IMT Anda</h1>
+              <Link href="/imt">
+                <Button className="capitalize">lihat data lain</Button>
               </Link>
             </div>
           </CardHeader>
@@ -41,6 +38,6 @@ export default function Home() {
           </CardContent>
         </Card>
       </div>
-    </main >
+    </main>
   );
 }
